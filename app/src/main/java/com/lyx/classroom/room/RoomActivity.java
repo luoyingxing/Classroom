f2b2b2ec296e0040ad4100258420fcb1a379967c
luoyingxing <luoyingxing@126.com>
2017-11-17 11:30:30 +0800

    update room model

--- app/src/main/java/com/lyx/classroom/room/RoomActivity.java
+++ app/src/main/java/com/lyx/classroom/room/RoomActivity.java
@@ -13,17 +13,19 @@ import com.lyx.frame.adapter.abs.CommonAdapter;
 import com.lyx.frame.adapter.abs.ViewHolder;
 import com.lyx.frame.annotation.Id;
 import com.lyx.frame.annotation.IdParser;
+import com.lyx.frame.utils.TimeUtils;
 
 import java.util.ArrayList;
 
 public class RoomActivity extends BaseActivity {
     public static final String PARAM = "AREA";
-    private Area mArea;
-
     @Id(R.id.lv_room)
     private ListView mListView;
 
+    private Area mArea;
     private CommonAdapter<Room> mAdapter;
+
+    private int[] mBackground = new int[]{R.color.bg_room_one, R.color.bg_room_two, R.color.bg_room_three, R.color.bg_room_four, R.color.bg_room_five};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,8 +40,16 @@ public class RoomActivity extends BaseActivity {
 
         mAdapter = new CommonAdapter<Room>(this, new ArrayList<Room>(), R.layout.item_room_list) {
             @Override
-            protected void convert(ViewHolder holder, Room room, int i) {
+            protected void convert(ViewHolder holder, Room room, int position) {
                 holder.setText(R.id.tv_room_title, room.getName());
+                holder.setText(R.id.tv_room_time, TimeUtils.showTime(room.getTime()));
+
+                holder.getView(R.id.iv_room_light).setVisibility(room.isHasLight() ? View.VISIBLE : View.GONE);
+                holder.getView(R.id.iv_room_air).setVisibility(room.isHasAir() ? View.VISIBLE : View.GONE);
+                holder.getView(R.id.iv_room_curtains).setVisibility(room.isHasCurtain() ? View.VISIBLE : View.GONE);
+                holder.getView(R.id.iv_room_breeze).setVisibility(room.isHasBreeze() ? View.VISIBLE : View.GONE);
+
+                holder.getView(R.id.tv_room_line).setBackgroundColor(getResources().getColor(mBackground[position % 5]));
             }
         };
 
